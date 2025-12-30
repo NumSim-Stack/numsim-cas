34ed1c7e9b0eb3390d8f42df3ebbe9d8fe73cd7e
petlenz <peterlenz89.pl@gmail.com>
2025-12-30 11:26:16 +0100

    add tensor_to_scalar tests; bug fixes

--- include/numsim_cas/tensor_to_scalar/simplifier/tensor_to_scalar_simplifier_div.h
+++ include/numsim_cas/tensor_to_scalar/simplifier/tensor_to_scalar_simplifier_div.h
@@ -50,29 +50,23 @@ public:
   // see operator overload operator()
   // using base::operator();
 
-  // lhs_tensor_to_scalar_with_scalar_div / rhs_tensor_to_scalar_with_scalar_div
-  // (lhs_tensor_to_scalar / lhs_scalar) / (rhs_tensor_to_scalar / rhs_scalar)
-  // (lhs_tensor_to_scalar * rhs_scalar) / (lhs_scalar * rhs_tensor_to_scalar)
+  // (A/a) / (B/b) = (A*b)/(a*B)
   constexpr inline expr_type
   operator()(tensor_to_scalar_with_scalar_div<value_type> const &rhs) {
-    return (rhs.expr_rhs() / m_data.expr_rhs()) *
-           (m_data.expr_lhs() / rhs.expr_lhs());
-  }
-
-  // lhs_tensor_to_scalar_with_scalar_div / scalar_with_tensor_to_scalar_div
-  // (lhs_tensor_to_scalar / lhs_scalar) / (rhs_scalar / rhs_tensor_to_scalar)
-  // lhs_tensor_to_scalar * rhs_tensor_to_scalar / (lhs_scalar*rhs_scalar)
-  constexpr inline expr_type
-  operator()(scalar_with_tensor_to_scalar_div<value_type> const &rhs) {
-    return m_data.expr_lhs() * rhs.expr_rhs() /
+    return (m_data.expr_lhs() * rhs.expr_rhs()) /
            (m_data.expr_rhs() * rhs.expr_lhs());
   }
 
-  // lhs_tensor_to_scalar_with_scalar_div / rhs_tensor_to_scalar
-  // (lhs_tensor_to_scalar / scalar) / rhs_tensor_to_scalar
-  // (lhs_tensor_to_scalar / (scalar * rhs_tensor_to_scalar)
+  // (A/a) / (b/B) = (A*B)/(a*b)  (your existing one is OK)
   constexpr inline expr_type
-  operator()(tensor_to_scalar_expression<value_type> const &) {
+  operator()(scalar_with_tensor_to_scalar_div<value_type> const &rhs) {
+    return (m_data.expr_lhs() * rhs.expr_rhs()) /
+           (m_data.expr_rhs() * rhs.expr_lhs());
+  }
+
+  // generic RHS: (A/a)/R = A/(a*R)
+  template <class RhsNode>
+  constexpr inline expr_type operator()(RhsNode const &) {
     return m_data.expr_lhs() /
            (m_data.expr_rhs() * std::forward<ExprRHS>(m_rhs));
   }
@@ -100,39 +94,68 @@ public:
 
   // using base::operator();
 
-  // scalar_with_tensor_to_scalar_div / rhs_tensor_to_scalar
-  // (scalar / lhs_tensor_to_scalar) / rhs_tensor_to_scalar
-  // scalar / (lhs_tensor_to_scalar / rhs_tensor_to_scalar)
-  constexpr inline expr_type
-  operator()(tensor_to_scalar_expression<value_type> const &) {
-    return m_data.expr_lhs() /
-           (std::forward<ExprRHS>(m_rhs) * m_data.expr_rhs());
-  }
-
-  // scalar_with_tensor_to_scalar_div / scalar_with_tensor_to_scalar_div
-  // (lhs_scalar / lhs_tensor_to_scalar) / (rhs_scalar / rhs_tensor_to_scalar)
-  // (lhs_scalar * rhs_tensor_to_scalar) / (lhs_tensor_to_scalar * rhs_scalar)
-  // (lhs_scalar / rhs_scalar) * (rhs_tensor_to_scalar / lhs_tensor_to_scalar)
-  // a/b/(x/y) = a*y/b*x
+  // (a/A) / (d/B) = (a/d)*(B/A)  (your existing rule is OK)
   constexpr inline expr_type
   operator()(scalar_with_tensor_to_scalar_div<value_type> const &rhs) {
     return (m_data.expr_lhs() / rhs.expr_lhs()) *
            (rhs.expr_rhs() / m_data.expr_rhs());
   }
 
-  // scalar_with_tensor_to_scalar_div / tensor_to_scalar_with_scalar_div
-  // (lhs_scalar / lhs_tensor_to_scalar) / (rhs_tensor_to_scalar / rhs_scalar)
-  // (lhs_scalar * rhs_scalar * ( lhs_tensor_to_scalar / rhs_tensor_to_scalar)
+  // (a/A) / (B/b) = (a*b)/(A*B)  FIXED
   constexpr inline expr_type
   operator()(tensor_to_scalar_with_scalar_div<value_type> const &rhs) {
-    return (m_data.expr_lhs() * rhs.expr_rhs()) *
-           (m_data.expr_rhs() / rhs.expr_lhs());
+    return (m_data.expr_lhs() * rhs.expr_rhs()) /
+           (m_data.expr_rhs() * rhs.expr_lhs());
+  }
+
+  // generic RHS: (a/A)/R = a/(A*R)
+  template <class RhsNode>
+  constexpr inline expr_type operator()(RhsNode const &) {
+    return m_data.expr_lhs() /
+           (m_data.expr_rhs() * std::forward<ExprRHS>(m_rhs));
   }
 
 private:
   using base::m_lhs;
   using base::m_rhs;
   scalar_with_tensor_to_scalar_div<value_type> const &m_data;
+};
+
+template <typename ExprLHS, typename ExprRHS>
+class wrapper_scalar_mul_div final : public div_default<ExprLHS, ExprRHS> {
+public:
+  using value_type = typename std::remove_reference_t<
+      std::remove_const_t<ExprLHS>>::value_type;
+  using expr_type = expression_holder<tensor_to_scalar_expression<value_type>>;
+  using base = div_default<ExprLHS, ExprRHS>;
+
+  wrapper_scalar_mul_div(ExprLHS &&lhs, ExprRHS &&rhs)
+      : base(std::forward<ExprLHS>(lhs), std::forward<ExprRHS>(rhs)),
+        m_data(
+            m_lhs
+                .template get<tensor_to_scalar_with_scalar_mul<value_type>>()) {
+  }
+
+  // using base::operator();
+
+  // (A*c)/(B*d) = (c/d)*(A/B)  (your existing one is OK)
+  constexpr inline expr_type
+  operator()(tensor_to_scalar_with_scalar_mul<value_type> const &expr) {
+    return (m_data.expr_rhs() / expr.expr_rhs()) *
+           (m_data.expr_lhs() / expr.expr_lhs());
+  }
+
+  // generic RHS: (A*c)/R = c*(A/R)
+  template <class RhsNode>
+  constexpr inline expr_type operator()(RhsNode const &) {
+    return m_data.expr_rhs() *
+           (m_data.expr_lhs() / std::forward<ExprRHS>(m_rhs));
+  }
+
+private:
+  using base::m_lhs;
+  using base::m_rhs;
+  tensor_to_scalar_with_scalar_mul<value_type> const &m_data;
 };
 
 template <typename ExprLHS, typename ExprRHS> struct div_base {
@@ -164,6 +187,15 @@ template <typename ExprLHS, typename ExprRHS> struct div_base {
     auto &expr_rhs{*m_rhs};
     return visit(
         wrapper_scalar_div_div<ExprLHS, ExprRHS>(std::forward<ExprLHS>(m_lhs),
+                                                 std::forward<ExprRHS>(m_rhs)),
+        expr_rhs);
+  }
+
+  constexpr inline expr_type
+  operator()(tensor_to_scalar_with_scalar_mul<value_type> const &) {
+    auto &expr_rhs{*m_rhs};
+    return visit(
+        wrapper_scalar_mul_div<ExprLHS, ExprRHS>(std::forward<ExprLHS>(m_lhs),
                                                  std::forward<ExprRHS>(m_rhs)),
         expr_rhs);
   }
--- include/numsim_cas/tensor_to_scalar/visitors/tensor_to_scalar_differentiation.h
+++ include/numsim_cas/tensor_to_scalar/visitors/tensor_to_scalar_differentiation.h
@@ -212,7 +212,7 @@ public:
         }
         term = std::move(term) * it_in->second;
       }
-      sum += term;
+      sum += std::move(term);
     }
 
     m_result = std::move(sum);
@@ -233,7 +233,7 @@ public:
     for (auto &child : visitable.hash_map() | std::views::values) {
       auto d = diff(child, m_arg);
       if (d.is_valid()) {
-        sum += std::move(d);
+        sum = std::move(sum) + std::move(d);
       }
     }
     m_result = std::move(sum);
@@ -252,11 +252,11 @@ public:
    * zero.
    */
   void operator()(tensor_to_scalar_div<ValueType> const &visitable) {
-    auto g = visitable.expr_lhs();
-    auto h = visitable.expr_rhs();
+    auto g{visitable.expr_lhs()};
+    auto h{visitable.expr_rhs()};
 
-    auto dg = diff(g, m_arg);
-    auto dh = diff(h, m_arg);
+    auto dg{diff(g, m_arg)};
+    auto dh{diff(h, m_arg)};
 
     if (!dg.is_valid()) {
       dg = make_expression<tensor_zero<value_type>>(m_arg.get().dim(),
@@ -294,11 +294,12 @@ public:
     auto dg{diff(g, m_arg)};
     auto dh{diff(h, m_arg)};
 
-    if (is_same<tensor_zero<ValueType>>(dh)) {
-      // h is constant (w.r.t. m_arg)
+    // If dh == 0 -> exponent constant w.r.t. X
+    if (is_same<tensor_zero<value_type>>(dh)) {
+      // df/dX = h * g^(h-1) * dg
       m_result = h * std::pow(g, h - one) * dg;
     } else {
-      // general case
+      // df/dX = g^(h-1) * (h*dg + dh*log(g)*g)
       m_result = std::pow(g, h - one) * (h * dg + dh * std::log(g) * g);
     }
   }
@@ -313,7 +314,14 @@ public:
    * \f]
    */
   void operator()([[maybe_unused]] tensor_to_scalar_pow_with_scalar_exponent<
-                  ValueType> const &) {}
+                  ValueType> const &visitable) {
+    auto dg{diff(visitable.expr_lhs(), m_arg)};
+    if (!dg.is_valid()) {
+      return; // zero
+    }
+    const auto &one{get_scalar_one<ValueType>()};
+    m_result = std::pow(visitable.expr_rhs(), visitable.expr_rhs() - one) * dg;
+  }
 
   /**
    * @brief Scalar multiplication: \f$ f = c\cdot g(\mathbf{X}) \f$.
@@ -322,7 +330,7 @@ public:
    */
   void
   operator()(tensor_to_scalar_with_scalar_mul<ValueType> const &visitable) {
-    auto dg = diff(visitable.expr_rhs(), m_arg);
+    auto dg{diff(visitable.expr_rhs(), m_arg)};
     if (!dg.is_valid()) {
       return; // zero
     }
@@ -346,7 +354,7 @@ public:
    */
   void
   operator()(tensor_to_scalar_with_scalar_div<ValueType> const &visitable) {
-    auto dg = diff(visitable.expr_lhs(), m_arg);
+    auto dg{diff(visitable.expr_lhs(), m_arg)};
     if (!dg.is_valid()) {
       return; // zero
     }
@@ -417,11 +425,11 @@ public:
    */
   void operator()(
       [[maybe_unused]] tensor_to_scalar_log<ValueType> const &visitable) {
-    // auto dg = diff(visitable.expr(), m_arg);
-    // if (!dg.is_valid()) {
-    //   return; // zero
-    // }
-    // m_result = std::move(dg) / visitable.expr();
+    auto dg = diff(visitable.expr(), m_arg);
+    if (!dg.is_valid()) {
+      return; // zero
+    }
+    m_result = std::move(dg) / visitable.expr();
   }
 
   /**
@@ -462,201 +470,3 @@ private:
 } // namespace numsim::cas
 
 #endif // TENSOR_TO_SCALAR_DIFFERENTIATION_H
-
-// #ifndef TENSOR_TO_SCALAR_DIFFERENTIATION_H
-// #define TENSOR_TO_SCALAR_DIFFERENTIATION_H
-
-// #include "../../numsim_cas_type_traits.h"
-// #include "../../operators.h"
-// #include "../../printer_base.h"
-// #include "../../scalar/visitors/scalar_printer.h"
-// #include "../../tensor/tensor_functions_fwd.h"
-// #include "../../tensor/visitors/tensor_printer.h"
-// #include <algorithm>
-// #include <functional>
-// #include <iostream>
-// #include <string>
-// #include <variant>
-// #include <vector>
-
-// namespace numsim::cas {
-
-// template <typename ValueType> class tensor_to_scalar_differentiation {
-// public:
-//   using value_type = ValueType;
-//   using expr_type =
-//   expression_holder<tensor_to_scalar_expression<value_type>>; using
-//   result_expr_type = expression_holder<tensor_expression<value_type>>;
-
-//   explicit tensor_to_scalar_differentiation(result_expr_type const &arg)
-//       : m_arg(arg) {
-//     I = make_expression<kronecker_delta<ValueType>>(arg.get().dim());
-//   }
-//   tensor_to_scalar_differentiation(tensor_to_scalar_differentiation const &)
-//   =
-//       delete;
-//   tensor_to_scalar_differentiation(tensor_to_scalar_differentiation &&) =
-//       delete;
-//   const tensor_to_scalar_differentiation &
-//   operator=(tensor_to_scalar_differentiation const &) = delete;
-
-//   auto apply(expr_type const &expr) {
-//     if (expr.is_valid()) {
-//       m_expr = expr;
-//       std::visit([this](auto &&arg) { (*this)(arg); }, *expr);
-//     }
-//     if (!m_result.is_valid()) {
-//       return make_expression<tensor_zero<value_type>>(m_arg.get().dim(),
-//                                                       m_arg.get().rank());
-//     }
-//     return m_result;
-//   }
-
-//   // trace(expr) = I:expr/dim(expr) = expr_ii/dim(expr)
-//   // dtrace(expr)/dX = dtrace(expr)/dexpr:dexpr/dX
-//   //                 = I:dexpr
-//   void operator()([[maybe_unused]] tensor_trace<ValueType> const &visitable)
-//   {
-//     auto result{diff(visitable.expr(), m_arg)};
-//     m_result = inner_product(I, sequence{1, 2}, result, sequence{1, 2});
-//   }
-
-//   // d(expr_ij expr_ij)/dx_k = dexpr_ij/dx_k*expr_ij + expr_ij*dexpr_ij/dx_k
-//   //                         = 2*expr [*] dexpr/dx
-//   void operator()(tensor_dot<ValueType> const &visitable) {
-//     auto result{diff(visitable.expr(), m_arg)};
-//     if (result.is_valid()) {
-//       constexpr auto two{static_cast<value_type>(2)};
-//       if (visitable.expr().get().rank() == 1) {
-//         m_result = two * visitable.expr() * std::move(result);
-//       }
-//       sequence indices(visitable.expr().get().rank());
-//       std::iota(indices.begin(), indices.end(), 1);
-//       m_result = two * inner_product(visitable.expr(), indices,
-//                                      std::move(result), indices);
-//     }
-//   }
-
-//   // dnorm(expr)/dX = dsqrt(dot(expr,expr))/dX
-//   //                = dnorm(expr)/dexpr : dexpr/dX
-//   //                = expr/norm(expr) : dexpr/dX
-//   void operator()([[maybe_unused]] tensor_norm<ValueType> const &visitable) {
-//     auto result{diff(visitable.expr(), m_arg)};
-//     if (result.is_valid()) {
-//       m_result = inner_product(visitable.expr(), sequence{1, 2}, result,
-//                                sequence{1, 2}) /
-//                  m_expr;
-//     }
-//   }
-
-//   // ddet(expr)/dX = ddet(expr)/dexpr:dexpr/dX
-//   //               = det(expr)*inv(trans(expr)):dexpr/dX
-//   void operator()([[maybe_unused]] tensor_det<ValueType> const &visitable) {
-//     auto result{diff(visitable.expr(), m_arg)};
-//     if (result.is_valid()) {
-//       m_result = m_expr * inner_product(inv(trans(visitable.expr())),
-//                                         sequence{1, 2}, result, sequence{1,
-//                                         2});
-//     }
-//   }
-
-//   // d(-expr) = -dexpr
-//   void operator()(tensor_to_scalar_negative<ValueType> const &visitable) {
-//     m_result = -diff(visitable.expr(), m_arg);
-//   }
-
-//   /// product rule
-//   /// f(x)  = c * prod_i^n a_i(x)
-//   /// f'(x)  = c * sum_j^n a_j(x) prod_i^{n, i\neq j} a_i(x)
-//   void operator()(
-//       [[maybe_unused]] tensor_to_scalar_mul<ValueType> const &visitable) {
-//     result_expr_type expr_result;
-//     for (auto &expr_out : visitable.hash_map() | std::views::values) {
-//       result_expr_type expr_result_in;
-//       // first get the diff
-//       expr_result_in *= diff(expr_out, m_arg);
-
-//       // now check if the diff is valid and not zero
-//       if (expr_result_in.is_valid() &&
-//           is_same<tensor_zero<value_type>>(expr_result_in)) {
-//         for (auto &expr_in : visitable.hash_map() | std::views::values) {
-//           expr_result_in = std::move(std::move(expr_result_in) * expr_in);
-//         }
-//       }
-//       expr_result += expr_result_in;
-//     }
-//     m_result = std::move(expr_result);
-//   }
-
-//   /// summation rule
-//   /// f(x)  = c + sum_i^n a_i(x)
-//   /// f'(x) = sum_i^n a_i'(x)
-//   void operator()(
-//       [[maybe_unused]] tensor_to_scalar_add<ValueType> const &visitable) {
-//     result_expr_type expr_result;
-//     for (auto &child : visitable.hash_map() | std::views::values) {
-//       expr_result += diff(child, m_arg);
-//     }
-//     m_result = std::move(expr_result);
-//   }
-
-//   void operator()(
-//       [[maybe_unused]] tensor_to_scalar_sub<ValueType> const &visitable) {}
-
-//   /// f(x) = g(x)/h(x)
-//   /// f(x) = (g(x)'*h(x) - g(x)*h(x)')/(h(x)*h(x)))
-//   /// g(x) := 0
-//   /// f(x) = (h(x) - g(x)*h(x)')/(h(x)*h(x)))
-//   /// h(x) := 0
-//   /// f(x) = (g(x)'*h(x) - g(x))/(h(x)*h(x)))
-//   void operator()(tensor_to_scalar_div<ValueType> const &visitable) {
-//     auto g{visitable.expr_lhs()};
-//     auto h{visitable.expr_rhs()};
-//     auto dg{diff(visitable.expr_lhs(), m_arg)};
-//     auto dh{diff(visitable.expr_rhs(), m_arg)};
-//     m_result = (dg * h - g * dh) / (h * h);
-//   }
-
-//   void operator()(
-//       [[maybe_unused]] tensor_to_scalar_pow<ValueType> const &visitable) {}
-
-//   void operator()([[maybe_unused]] tensor_to_scalar_pow_with_scalar_exponent<
-//                   ValueType> const &visitable) {}
-
-//   void
-//   operator()(tensor_to_scalar_with_scalar_mul<ValueType> const &visitable) {
-//     m_result = visitable.expr_lhs() * diff(visitable.expr_rhs(), m_arg);
-//   }
-
-//   void
-//   operator()(tensor_to_scalar_with_scalar_add<ValueType> const &visitable) {
-//     m_result = diff(visitable.expr_rhs(), m_arg);
-//   }
-
-//   void
-//   operator()(tensor_to_scalar_with_scalar_div<ValueType> const &visitable) {
-//     m_result = diff(visitable.expr_lhs(), m_arg) / visitable.expr_rhs();
-//   }
-
-//   void
-//   operator()([[maybe_unused]] scalar_with_tensor_to_scalar_div<ValueType>
-//   const
-//                  &visitable) {}
-
-//   void
-//   operator()([[maybe_unused]] tensor_inner_product_to_scalar<ValueType> const
-//                  &visitable) {}
-
-//   template <typename Expr>
-//   void operator()([[maybe_unused]] Expr const &visitable) {}
-
-// private:
-//   result_expr_type const &m_arg;
-//   result_expr_type m_result{nullptr};
-//   expr_type m_expr;
-//   result_expr_type I;
-// };
-
-// } // namespace numsim::cas
-
-// #endif // TENSOR_TO_SCALAR_DIFFERENTIATION_H
